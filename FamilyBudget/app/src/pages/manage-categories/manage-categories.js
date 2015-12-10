35771562b85f7b1fde6ad5b1fe2fc8b1ca2df086
Maksim Murzin <Maksim_Murzin@epam.com>
2015-12-10 13:45:26 +0300

    Added FE actions for manage-categories page

--- FamilyBudget/app/src/pages/manage-categories/manage-categories.js
+++ FamilyBudget/app/src/pages/manage-categories/manage-categories.js
@@ -3,9 +3,43 @@
 
     angular.module('App').controller('ManageCategoriesCtrl', ManageCategoriesCtrl);
 
-    ManageCategoriesCtrl.$inject = ['$scope', 'categories'];
+    ManageCategoriesCtrl.$inject = ['$scope', 'categories', '$timeout'];
 
-    function ManageCategoriesCtrl($scope, categories) {
+    function ManageCategoriesCtrl($scope, categories, $timeout) {
         $scope.categories = categories.data;
+        $scope.isEditing = [];
+        $scope.editing = {
+            value: ''
+        };
+        $scope.startEditing = function (index) {
+            $scope.isEditing[index] = true;
+            $scope.editing.value = $scope.categories[index] ? $scope.categories[index].Name : '';
+        };
+
+        $scope.cancelEditing = function (index) {
+            $scope.isEditing[index] = false;
+            $scope.editing.value = '';
+        };
+
+        $scope.applyEditing = function (index) {
+            var category = $scope.categories[index];
+            if (category && _.isString($scope.editing.value) && $scope.editing.value.length > 1) {
+                category.Name = $scope.editing.value;
+                $scope.editing.value = '';
+                $scope.isEditing[index] = false;
+            }
+        }
+
+        $scope.deleteCategory = function (index) {
+            if (index >= 0 && index < $scope.categories.length) {
+                $scope.categories.splice(index, 1);
+            }
+        }
+
+        function init() {
+            if (_.isArray($scope.categories)) {
+                _.fill($scope.isEditing, false, 0, $scope.categories.length);
+            }
+        }
     }
 })();
